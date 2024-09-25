c2a2e694f8f197ac2fa60dda84f1ab793d152670
slotaddictcounselor <182563604+slotaddictcounselor@users.noreply.github.com>
2024-09-25 23:39:12 +0200

    zabka-snrs authentication

--- superlogin.py
+++ superlogin.py
@@ -6,8 +6,9 @@ import uuid
 
 class auth:
     """
-    All functions used for authentication.
+    Functions related to authentication.
     """
+
     def get_temp_auth_token():
         """
         Used for phone authentication.
@@ -94,5 +95,75 @@ class auth:
             
         if response.json()['data']['signIn']['customToken']:
             return response.json()['data']['signIn']['customToken']
+        else:
+            return False
+        
+    def verify_custom_token(token):
+        """
+        Requires token received after phone authentication.
+        Returns yet another token and refreshToken.
+        """
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/verifyCustomToken?key=AIzaSyDe2Fgxn_8HJ6NrtJtp69YqXwocutAoa9Q"
+
+        headers = {
+            "content-type": "application/json",
+        }
+
+        data = {
+            "token": token,
+            "returnSecureToken": "True",
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+
+        if response.json()['idToken']:
+            return response.json()['idToken']
+        else:
+            return False
+        
+    def get_account_info(token):
+        url = "https://www.googleapis.com/identitytoolkit/v3/relyingparty/getAccountInfo?key=AIzaSyDe2Fgxn_8HJ6NrtJtp69YqXwocutAoa9Q"
+
+        headers = {
+            "content-type": "application/json"
+        }
+
+        data = {
+            "idToken": token,
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        print(response.json())
+
+        return response.json()
+        
+    def get_main_token(token):
+        uid = str(uuid.uuid4())
+        print(uid)
+        apiKey = "00000000-0000-0000-0000-000000000000" # Not provided.
+
+        url = "https://zabka-snrs.zabka.pl/sauth/v3/auth/login/client/conditional"
+
+        headers = {
+            "api-version": "4.4",
+            "application-id": "%C5%BCappka",
+            "user-agent": "Synerise Android SDK 5.9.0 pl.zabka.apb2c",
+            "accept": "application/json",
+            "content-type": "application/json; charset=UTF-8",
+            "mobile-info": "horizon;28;AW700000000;9;CTR-001;nintendo;5.9.0",
+        }
+
+        data = {
+            "identityProviderToken": token,
+            "identityProvider": "OAUTH",
+            "apiKey": apiKey,
+            "uuid": uid,
+            "deviceId": "0432b18513e325a5",
+        }
+
+        response = requests.post(url, headers=headers, data=json.dumps(data))
+        
+        if response.json()['token']:
+            return response.json()['token']
         else:
             return False
--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import superlogin
 
+# Get token needed for phone authorization
 temp_auth_token = superlogin.auth.get_temp_auth_token()
 
 phone_number = input("Enter phone number (ex. 123456789): ")
@@ -7,6 +8,7 @@ country_code = input("Enter country code (ex. 48): ")
 
 print("Sending SMS authentication request.")
 
+# Send phone authorization request. Sends a message to given phone number.
 superlogin.auth.phone_auth_init(temp_auth_token, country_code, phone_number)
 
 print(f"+{country_code}{phone_number} should receive a message with a verification code.")
@@ -17,5 +19,27 @@ token = superlogin.auth.phone_auth(temp_auth_token, country_code, phone_number, 
 
 if token == False:
     print("Something went wrong during authentication (no token in response)")
+    exit()
 else:
     print(f"Successfully authenticated!")
+
+print("Verifying custom token.")
+
+custom_token = superlogin.auth.verify_custom_token(token)
+
+if custom_token == False:
+    print("Something went wrong during verification (idToken or refreshToken missing)")
+    exit()
+else:
+    print("Custom token verified.")
+
+print("Retrieving main token.")
+
+superlogin.auth.get_account_info(custom_token)
+
+main_token = superlogin.auth.get_main_token(custom_token)
+
+if main_token == False:
+    print("Something went wrong while retrieving the main token.")
+else:
+    print("Main token received.")
