6104d44ad23c3ae7577c362b641de2768490c481
slotaddictcounselor <182563604+slotaddictcounselor@users.noreply.github.com>
2024-09-25 20:25:30 +0200

    Moved authentication functions to auth class
    Also removed print for auth functions in test script.

--- test.py
+++ test.py
@@ -1,16 +1,16 @@
 import superlogin
 
-temp_auth_token = superlogin.get_temp_auth_token()
+temp_auth_token = superlogin.auth.get_temp_auth_token()
 
 phone_number = input("Enter phone number (ex. 123456789): ")
 country_code = input("Enter country code (ex. 48): ")
 
 print("Sending SMS authentication request.")
 
-print(superlogin.phone_auth_init(temp_auth_token, country_code, phone_number))
+superlogin.auth.phone_auth_init(temp_auth_token, country_code, phone_number)
 
 print(f"+{country_code}{phone_number} should receive a message with a verification code.")
 
 sms_code = input("Enter received SMS code: ")
 
-print(superlogin.phone_auth(temp_auth_token, country_code, phone_number, sms_code))
+superlogin.auth.phone_auth(temp_auth_token, country_code, phone_number, sms_code)
