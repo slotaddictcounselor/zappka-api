366b7479d49d2bbef764935dff16e41aab9a32ef
slotaddictcounselor <182563604+slotaddictcounselor@users.noreply.github.com>
2024-09-26 16:35:39 +0200

    Name change

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
-import superlogin
+import zappka
 
 # Get token needed for phone authorization
-temp_auth_token = superlogin.auth.get_temp_auth_token()
+temp_auth_token = zappka.auth.get_temp_auth_token()
 
 phone_number = input("Enter phone number (ex. 123456789): ")
 country_code = input("Enter country code (ex. 48): ")
@@ -9,13 +9,13 @@ country_code = input("Enter country code (ex. 48): ")
 print("Sending SMS authentication request.")
 
 # Send phone authorization request. Sends a message to given phone number.
-superlogin.auth.phone_auth_init(temp_auth_token, country_code, phone_number)
+zappka.auth.phone_auth_init(temp_auth_token, country_code, phone_number)
 
 print(f"+{country_code}{phone_number} should receive a message with a verification code.")
 
 sms_code = input("Enter received SMS code: ")
 
-token = superlogin.auth.phone_auth(temp_auth_token, country_code, phone_number, sms_code)
+token = zappka.auth.phone_auth(temp_auth_token, country_code, phone_number, sms_code)
 
 if token == False:
     print("Something went wrong during authentication (no token in response)")
@@ -25,7 +25,7 @@ else:
 
 print("Verifying custom token.")
 
-identityProviderToken = superlogin.auth.verify_custom_token(token)
+identityProviderToken = zappka.auth.verify_custom_token(token)
 
 if identityProviderToken == False:
     print("Something went wrong during verification (idToken or refreshToken missing)")
@@ -33,6 +33,6 @@ if identityProviderToken == False:
 else:
     print("Custom token verified.")
 
-superlogin.auth.get_account_info(identityProviderToken)
+zappka.auth.get_account_info(identityProviderToken)
 
-print(superlogin.account.get_details(identityProviderToken))
+print(zappka.account.get_details(identityProviderToken))
